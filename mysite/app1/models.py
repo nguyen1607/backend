746229c5a65432563581ead131d974904daeff83
F2-TE-Mem-Nguyen.Tran <nguyen.tran@spclt.com.vn>
2020-11-10 16:57:16 +0700

    Example

--- mysite/app1/models.py
+++ mysite/app1/models.py
@@ -13,6 +13,6 @@ class Question(models.Model):
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length = 200)
-    votes = models.IntegerField(default=0) 
+    votes = models.IntegerField(default=0)
     def __str__(self):
-        return self.question_text
+        return self.choice_text
--- mysite/app1/views.py
+++ mysite/app1/views.py
@@ -5,3 +5,12 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse('Hello')
 # Create your views here.
+def detail(request, question_id):
+    return HttpResponse('You look at question %s' % question_id)
+
+def results(request, question_id):
+    respone = 'You look at the results %s'
+    return HttpResponse(respone % question_id)
+    
+def vote(request, question_id):
+    return HttpResponse('You vote on question %s' % question_id)
