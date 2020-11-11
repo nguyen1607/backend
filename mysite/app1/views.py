715f366445508b38202447fd16a1f5b784eac3f4
F2-TE-Mem-Nguyen.Tran <nguyen.tran@spclt.com.vn>
2020-11-11 17:24:21 +0700

    update

--- mysite/app1/views.py
+++ mysite/app1/views.py
@@ -1,12 +1,27 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
+from django.template import loader
+from app1.models import Choice, Question
 
 
 def index(request):
-    return HttpResponse('Hello')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    #template = loader.get_template('app1/index.html')
+    context = {
+        'latest_question_list' : latest_question_list,
+    }
+    #return HttpResponse(template.render(context, request))
+    return render(request, 'app1/index.html', context)
 # Create your views here.
 def detail(request, question_id):
-    return HttpResponse('You look at question %s' % question_id)
+    """
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404('Question does not exist')
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'app1/detail.html', {'question':question})
 
 def results(request, question_id):
     respone = 'You look at the results %s'
