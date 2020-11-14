0a1a11c6b890014c2f870240a171e2a1fb7878af
F2-TE-Mem-Nguyen.Tran <nguyen.tran@spclt.com.vn>
2020-11-14 15:53:43 +0700

    Add polls

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'vrcfqftq!(5hag4qsm_dlxve-%t4cz_--tzw4z@#nw%p*t#b9!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app1.apps.App1Config', 
+    'polls.apps.PollsConfig',
 ]
 
 MIDDLEWARE = [
